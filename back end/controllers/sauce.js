0634440ef086b17c3a919a008660760a51e1c131
Thomas Landais <thomas.landais9733@gmail.com>
2021-09-19 19:05:35 +0200

    ajout modify

--- back end/controllers/sauce.js
+++ back end/controllers/sauce.js
@@ -1,5 +1,6 @@
 const Sauce = require("../models/Sauce");
 const user = require("../models/User");
+const fs = require('fs');
 
 exports.getAll = (req, res, next) => {
     Sauce.find()
@@ -30,15 +31,56 @@ exports.create = (req, res, next) => {
 };
 
 exports.modify = (req, res, next) => {
-
+    let sauceObj = {};
+    if (req.file) {
+        Sauce.findOne({ _id: req.params.id })
+            .then((sauce) => {
+                const name = sauce.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${name}`, (err) => {
+                    if (err) console.log(err);
+                    else {
+                        console.log("ancienne photo supprimée");
+                    }
+                })
+            })
+            .catch(error => res.status(400).json({ error }));
+        sauceObj = {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        };
+    } else {
+        sauceObj = { ...req.body };
+    }
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObj, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+        .catch(error => res.status(400).json({ message: error }));
 };
 
 exports.delete = (req, res, next) => {
-    Sauce.findOneAndDelete({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-        .catch(error => res.status(400).json({ error }));
+    Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+            const name = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${name}`, () => {
+                Sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+                    .catch((e) => res.status(400).json({ message: e }))
+            })
+        })
+        .catch(error => res.status(400).json({ message: error }));
 };
 
 exports.like = (req, res, next) => {
-
+    Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+            if (req.body.like > 0) {
+                sauce.likes += 1;
+                sauce.usersLiked.push(req.body.userId);
+            } else if (req.body.like <= 0) {
+                sauce.dislikes += -1;
+                sauce.usersDisliked.push(req.body.userId);
+            }
+            console.log(sauce.likes);
+            console.log(sauce.usersLiked);
+        })
+        .catch(error => res.status(400).json({ message: error }));
 };
