5cc6c7da69d8e5042f6176f6ffa0eb75ba1c9e43
Thomas Landais <thomas.landais9733@gmail.com>
2021-09-24 16:58:08 +0200

    ajout validation des input de la route modification

--- back end/middleware/input-validator.js
+++ back end/middleware/input-validator.js
@@ -1,10 +1,10 @@
 exports.postInput = (req, res, next) => {
     const { Validator } = require('node-input-validator');
     const v = new Validator(JSON.parse(req.body.sauce), {
-        name: 'required|minLength:3',
-        manufacturer: 'required|minLength:3',
-        description: 'required|minLength:3',
-        mainPepper: 'required|minLength:3',
+        name: 'required|minLength:3|string',
+        manufacturer: 'required|minLength:3|string',
+        description: 'required|minLength:3|string',
+        mainPepper: 'required|minLength:3|string',
     });
     v.check().then((matched) => {
         console.log(matched);
@@ -18,12 +18,22 @@ exports.postInput = (req, res, next) => {
 
 exports.modifyInput = (req, res, next) => {
     const { Validator } = require('node-input-validator');
-    const v = new Validator(req.body, {
-        name: 'required|minLength:3',
-        manufacturer: 'required|minLength:3',
-        description: 'required|minLength:3',
-        mainPepper: 'required|minLength:3',
-    });
+    let v;
+    if(!req.file){
+        v = new Validator(req.body, {
+            name: 'required|minLength:3|string',
+            manufacturer: 'required|minLength:3|string',
+            description: 'required|minLength:3|string',
+            mainPepper: 'required|minLength:3|string',
+        });
+    }else{
+        v = new Validator(JSON.parse(req.body.sauce), {
+            name: 'required|minLength:3|string',
+            manufacturer: 'required|minLength:3|string',
+            description: 'required|minLength:3|string',
+            mainPepper: 'required|minLength:3|string',
+        });
+    }
     v.check().then((matched) => {
         console.log(matched);
         if (!matched) {
